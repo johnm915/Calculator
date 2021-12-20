42ff6817d51474076141e31451d4491255feb767
john morgangit config --global user.email jmm915@gmail.com <jmm915@gmail.com>
2021-12-20 11:06:55 -0600

    first commit

---
+++ Practice Calculator 1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Practice_Calculator_1
+{
+    class Program
+    {
+        static void Main (string[] args)
+
+        {
+            Console.WriteLine("What is your name?");
+            string userName = Console.ReadLine();
+            Console.WriteLine($"Plaese enter a number");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a second number to add to the first number");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(num1 + num2);
+            
+
+
+
+        } 
+
+    }
+}
+
